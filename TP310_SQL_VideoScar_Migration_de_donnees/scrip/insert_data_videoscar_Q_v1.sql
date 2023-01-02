fbb58cd49071d1ffc1b70c138ef4012bb0521437
Adrien <103360104+AdHouen@users.noreply.github.com>
2023-01-02 22:23:41 +0100

    310

--- TP310_SQL_VideoScar_Migration_de_donnees/scrip/insert_data_videoscar_Q_v1.sql
+++ TP310_SQL_VideoScar_Migration_de_donnees/scrip/insert_data_videoscar_Q_v1.sql
@@ -1,23 +1,25 @@
-												
+					
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'DeChauveron','Philippe');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Qu est-ce quon à fait au on dieu	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Qu est-ce quon à fait au on dieu', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Bob','REINER');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quand Harry rencontre Sally	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Quand Harry rencontre Sally', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Xavier','GIANNOLI');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quand j étais Chanteur	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Quand j étais Chanteur', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Gilles','PORTE');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quand la mer monte	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'uand la mer monte', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Christian','VINCENT');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quatre étoiles	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Quatre étoiles', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Cristian','MUNGIU');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quatre mois, 3 semaines et 2jours	', 1800, 'FRA', null	);
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quatre mois, 3 semaines, 2 jours	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Quatre mois, 3 semaines et 2jours', 1800, 'F', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Quatre mois, 3 semaines, 2 jours', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Patrick','TIMSIT');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quelqu un de bien	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Quelqu un de bien', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Santiago','AMIGORENA');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Quelques jours en Septembre	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Quelques jours en Septembre', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Gilles','MARCHAND');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Qui à tué Bambi ?	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Qui à tué Bambi ?', 1800, 'F', null	);
 INSERT INTO 	realise	(reacod, reanom, reapre)	VALUES	(	seq_realise.nextval, 'Robert','ZEMECKIS');
-INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'	Qui veut la peau de Roger Rabbit	', 1800, 'FRA', null	);
+INSERT INTO 	film	(filmcod,  catcod, reacod, filmtit, filman, filnat, filmdur)	VALUES	(	seq_film.nextval, 	null,	seq_realise.currval		,'Qui veut la peau de Roger Rabbit', 1800, 'F', null	);
+
+commit;
 												
